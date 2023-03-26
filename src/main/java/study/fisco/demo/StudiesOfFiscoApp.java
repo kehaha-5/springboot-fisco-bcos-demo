be64278eb0c9b61d823d97e4597df9e8a3777bce
kehaha <1011700940@qq.com>
2023-03-26 22:05:01 +0800

    init

---
+++ src/main/java/study/fisco/demo/StudiesOfFiscoApp.java
@@ -0,0 +1,12 @@
+package study.fisco.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesOfFiscoApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(StudiesOfFiscoApp.class,args);
+    }
+}
